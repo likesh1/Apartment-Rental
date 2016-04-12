c96c12f02b486819c4269e20285b1790900ab163
Pritam <pborate@uncc.edu>
2016-04-12 05:37:47 -0400

    Junit Test case updated for Registration

--- ApartmentsNearUNCC/src/test/java/org/apartments/uncc/dao/impl/test/TestUserDao.java
+++ ApartmentsNearUNCC/src/test/java/org/apartments/uncc/dao/impl/test/TestUserDao.java
@@ -5,6 +5,8 @@ package org.apartments.uncc.dao.impl.test;
 
 import static org.junit.Assert.*;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
@@ -14,7 +16,9 @@ import org.apartments.uncc.dao.impl.UserDaoImpl;
 import org.apartments.uncc.dao.impl.stub.UserDaoImplStub;
 import org.apartments.uncc.dao.impl.stub.UserDaoStub;
 import org.apartments.uncc.exceptions.InvalidCredentialsException;
+import org.apartments.uncc.exceptions.InvalidEmailIdException;
 import org.apartments.uncc.viewBeans.LoginBean;
+import org.apartments.uncc.viewBeans.RegistrationBean;
 import org.apartments.uncc.viewBeans.UserDetailsBean;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -45,9 +49,11 @@ public class TestUserDao {
 	private ApplicationContext applicationContext;
 	@Autowired
 	private UserDao userDao;
+	@Autowired
+	DataSource dataSource;
 	private UserDetailsBean userBean;
 	private LoginBean loginBean;
-	
+	private RegistrationBean registrationBean;
 	
 
 	/**
@@ -70,9 +76,18 @@ public class TestUserDao {
 	@Before
 	public void setUp() throws Exception {
 		//userDao=new UserDaoImpl();
+		PreparedStatement pst=dataSource.getConnection().prepareStatement("insert into login(email_id,password,userRole) values(\"pritam.borate16@gmail.com\",\"pritam\",\"student\")");
+		pst.executeUpdate();
 		loginBean=new LoginBean();
-		loginBean.setUsername("pborate@uncc.edu");
+		loginBean.setUsername("pritam.borate16@gmail.com");
 		loginBean.setPassword("pritam");
+		//PreparedStatement pstmt=dataSource.getConnection().prepareStatement("");
+		registrationBean=new RegistrationBean();
+		registrationBean.setEmail("test123@test.com");
+		registrationBean.setFname("Test");
+		registrationBean.setLname("Name");
+		registrationBean.setPassword("Dummy");
+		registrationBean.setUserRole("owner");
 	}
 
 	/**
@@ -80,7 +95,37 @@ public class TestUserDao {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		PreparedStatement pstmt=dataSource.getConnection().prepareStatement("delete from apartmentOwner where oemail=\"test123@test.com\"");
+		pstmt.executeUpdate();
+		PreparedStatement pst=dataSource.getConnection().prepareStatement("delete from login where email_id IN (\"pritam.borate16@gmail.com\",\"test123@test.com\")");
+		pst.executeUpdate();
 	}
+	/**
+	 * Test method for {@link org.apartments.uncc.dao.impl.UserDaoImpl#isValidRegistration(org.apartments.uncc.viewBeans.RegistrationBean)}.
+	 * @throws InvalidEmailIdException 
+	 * @throws SQLException  
+	 */
+	@Test
+	public void testIsValidRegistration() throws SQLException, InvalidEmailIdException{
+		System.out.println("---Registration successful---"+userDao.isValidRegistration(registrationBean));
+		PreparedStatement pstmt=dataSource.getConnection().prepareStatement("select email_id from login where email_id=\"test123@test.com\"");
+		ResultSet resultSet=pstmt.executeQuery();
+		String login_email_id="",owner_email="";
+		while(resultSet.next())
+			{
+				login_email_id=resultSet.getString(1) ;
+			}
+		pstmt=dataSource.getConnection().prepareStatement("select oemail from apartmentOwner where oemail=\"test123@test.com\"");
+		ResultSet rs=pstmt.executeQuery();
+		while(rs.next())
+		{
+			owner_email=rs.getString(1);
+		}
+		assertEquals("test123@test.com", login_email_id);
+		assertEquals("test123@test.com", owner_email);
+		
+	}
+	
 
 	/**
 	 * Test method for {@link org.apartments.uncc.dao.impl.UserDaoImpl#isValidUser(java.lang.String, java.lang.String)}.
@@ -89,9 +134,9 @@ public class TestUserDao {
 	@Test
 	public void testIsValidUser() throws InvalidCredentialsException {
 		try {
-			System.out.println("Its Working!!!");
+			//System.out.println("Its Working!!!");
 			System.out.println("------"+userDao.isValidUser(loginBean));
-			assertEquals("pborate@uncc.edu",userDao.isValidUser(loginBean).getUsername());
+			assertEquals("Login Successful!!!","pritam.borate16@gmail.com",userDao.isValidUser(loginBean).getUsername());
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
